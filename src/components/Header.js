db5abff9bcf7a9a390eb64adc2c092dd7cf745c1
Vitória <vitcs11@gmail.com>
2023-01-11 18:53:16 -0300

    Requisito 2

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Header extends Component {
   render() {
+    const { email, totalExpenses, currentCurrency } = this.props;
     return (
-      <div>Header</div>
+      <div>
+        <p data-testid="email-field">{ email }</p>
+        <p data-testid="total-field">{ totalExpenses }</p>
+        <p data-testid="header-currency-field">{ currentCurrency }</p>
+
+      </div>
     );
   }
 }
 
-export default Header;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+  totalExpenses: state.wallet.totalExpenses,
+  currentCurrency: state.wallet.currentCurrency,
+});
+
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+  totalExpenses: PropTypes.number.isRequired,
+  currentCurrency: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Header);
--- src/redux/reducers/wallet.js
+++ src/redux/reducers/wallet.js
@@ -1,6 +1,9 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
 
-const INITIAL_STATE = {};
+const INITIAL_STATE = {
+  totalExpenses: 0,
+  currentCurrency: 'BRL',
+};
 
 const wallet = (state = INITIAL_STATE, action) => {
   switch (action.type) {
